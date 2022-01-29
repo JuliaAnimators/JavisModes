abeb6e9b77e8f666152dc84f54b264e166cdc1ee
John George Francis <arbitrandomuser@gmail.com>
2022-01-29 21:28:08 +0530

    formated timers.jl

--- timers.jl
+++ timers.jl
@@ -1,15 +1,15 @@
-module JavisTimer 
+module JavisTimer
 import Base
 
-export timer
+export timer, finterval
 
 framerate = 30
 currentframe = 1
 
 
 """
-module contains some functions for manipulating time directly 
-not that in general manipulating time is tricky because of 
+module contains some functions for manipulating time directly
+not that in general manipulating time is tricky because of
 discrete nature of frames.
 
 """
@@ -29,29 +29,29 @@ struct finterval #frame interval
   """
   beginframe::Int
   endframe::Int
-  iterable
+  iterable::Any
 end
 
-finterval(b::Int,e::Int) = finterval(b,e,b:e)
-finterval(e) = finterval(currentframe,e,currentframe:e)
+finterval(b::Int, e::Int) = finterval(b, e, b:e)
+finterval(e) = finterval(currentframe, e, currentframe:e)
 
 """implement iteration on finterval"""
-Base.iterate(f1::finterval) = Base.iterate(f1.iterable) 
-function Base.iterate(f1::finterval,state)
-  return Base.iterate(f1.iterable,state) 
+Base.iterate(f1::finterval) = Base.iterate(f1.iterable)
+function Base.iterate(f1::finterval, state)
+  return Base.iterate(f1.iterable, state)
 end
 
 
-function Base.:+(f1::finterval,f2::finterval)
+function Base.:+(f1::finterval, f2::finterval)
   """adds two intervals, addition is defined as an finterval from
   f1's first frame to f2's last frame
   """
-  return finterval(f1.beginframe,f2.endframe) 
+  return finterval(f1.beginframe, f2.endframe)
 end
 
-function Base.:*(x::Real,f1::finterval)
+function Base.:*(x::Real, f1::finterval)
   """scales the interval by x , keeps the beginframe the same"""
-  return finterval(f1.beginframe,floor(Int,x*f1.endframe))
+  return finterval(f1.beginframe, floor(Int, x * f1.endframe))
 end
 #another other ideas for * on finterval is to scale around midpoint frame
 
@@ -67,43 +67,42 @@ function timer(t::Real)
   to the current timestamp."""
   global currentframe
   beginframe = currentframe
-  endframe = currentframe + floor(Int,t*framerate) - 1
+  endframe = currentframe + floor(Int, t * framerate) - 1
   currentframe = endframe + 1
-  return finterval(beginframe,endframe)
+  return finterval(beginframe, endframe)
 end
 
 function timer(f::finterval)
-  """advance currentframe by the duration same as f, 
+  """advance currentframe by the duration same as f,
   and return apropriate finterval"""
-  global currentframe,
-  beginframe = currentframe
+  global currentframe, beginframe = currentframe
   Δframe = f.endframe - f.beginframe + 1
-  endframe = currentframe + Δframe 
-  return finterval(beginframe,endframe)
+  endframe = currentframe + Δframe
+  return finterval(beginframe, endframe)
 end
 
 function getframe()
   """ returns currentframe """
   global currentframe
-  return currentframe 
+  return currentframe
 end
 
 function setframe(f::Int)
   """ sets current frame to f """
   global currentframe
-  currentframe=f
+  currentframe = f
 end
 
 function gettime()
   """gets timestamp, time is tricky to work with.
   """
   global currentframe
-  return currentframe*framerate
+  return currentframe * framerate
 end
 
 function settime(t::Real)
   global currentframe
-  currentframe= floor(Int,s*framerate)
+  currentframe = floor(Int, s * framerate)
 end
 
 function settime(f::finterval)
@@ -114,4 +113,3 @@ function settime(f::finterval)
 end
 
 end
-
